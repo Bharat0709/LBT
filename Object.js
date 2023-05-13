ffe8c05da0c07d7b3bcd30aab20fd02c83d62d87
Bharat Pahwa <pahwabharat15@gmail.com>
2023-05-13 13:12:24 +0530

    Objects Added

---
+++ Object.js
@@ -0,0 +1,191 @@
+const GmailArray = [
+  {
+    url: "https://twitter.com/Wyatt_Beemer/status/1655543128017846274?s=20",
+    description: "",
+    credits: "Wyatt Beemer",
+  },
+  {
+    url: "https://twitter.com/thecurlypreneur/status/1656994958589538304?s=20",
+    description: "",
+    credits: "The Curlypreneur",
+  },
+  {
+    url: "https://twitter.com/Jenn_JDR/status/1653640339867959296?s=20",
+    description: "",
+    credits: "Jenn JDR",
+  },
+  {
+    url: "https://twitter.com/OPTIZMO/status/1656011653736022017?s=20",
+    description: "",
+    credits: "OPTIZMO",
+  },
+  {
+    url: "https://hbr.org/2016/09/a-guide-to-cold-emailing",
+    description: "",
+    credits: "Harvard Business Review",
+  },
+  {
+    url: "https://woodpecker.co/blog/how-to-write-a-cold-email-that-actually-works-six-step-tutorial/",
+    description: "",
+    credits: "Woodpecker",
+  },
+  {
+    url: "https://salesblink.io/blog/tips-to-write-cold-emails-that-get-responses",
+    description: "",
+    credits: "SalesBlink",
+  },
+  {
+    url: "https://www.saleshacker.com/best-cold-email-tips/",
+    description: "",
+    credits: "SalesHacker",
+  },
+  {
+    url: "https://blog.close.com/cold-email-tips-linkedin/",
+    description: "",
+    credits: "Close",
+  },
+  {
+    url: "https://www.leadfuze.com/how-to-write-a-cold-email/",
+    description: "",
+    credits: "LeadFuze",
+  },
+  {
+    url: "https://blog.klenty.com/cold-emailing-tips-from-sales-reps/",
+    description: "",
+    credits: "Klenty",
+  },
+  {
+    url: "https://www.linkedin.com/posts/snehasharmathecoach_how-to-write-effective-cold-emails-for-job-activity-7062637178941562880-N7_i?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Sneha Sharma",
+  },
+  {
+    url: "https://www.linkedin.com/posts/pratiksha-sharma-347902b5_saas-emailmarketing-coldoutreach-activity-7062388422425948160-eq0g?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Pratiksha Sharma",
+  },
+  {
+    url: "https://www.linkedin.com/posts/sattwik-das_coldemails-sales-marketing-activity-7061921774820614144-n9P7?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Sattwik Das",
+  },
+  {
+    url: "https://www.linkedin.com/posts/iamvinoj_stop-sending-unsuccessful-cold-emails-activity-7061180427981516800-xHHB?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Vinoj Kumar",
+  },
+  {
+    url: "https://www.linkedin.com/posts/pratiksha-sharma-347902b5_stop-sending-emails-manually-activity-7062826379003740161-_Re9?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Pratiksha Sharma",
+  },
+  {
+    url: "https://www.linkedin.com/posts/warikoo_coldemails-work-jobs-activity-7033805031581659136-W0YE?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Sumit Warikoo",
+  },
+  {
+    url: "https://www.linkedin.com/posts/activity-7059776849567989760-tsGc?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://www.linkedin.com/posts/vivek-patil-52381916_5-successful-tips-for-cold-emails-activity-7039966907994628096-Qykg?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Vivek Patil",
+  },
+  {
+    url: "https://www.linkedin.com/posts/activity-7021713630282870784-PXo9?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://www.linkedin.com/posts/vivek-patil-52381916_campaign-tips-for-cold-email-reachout-activity-7029860421230563328-wXfl?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Vivek Patil",
+  },
+  {
+    url: "https://www.linkedin.com/posts/yash-khemka_coldemails-leadgeneration-saas-activity-6971308230165561344-VBwr?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Y",
+  },
+
+  {
+    url: "https://www.linkedin.com/posts/yash-khemka_emailmarketing-coldemailing-coldemailoutreach-activity-7044616279650910210-dwXp?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Yash Khemka",
+  },
+  {
+    url: "https://www.linkedin.com/posts/muthukumarae_b2b-cold-emails-that-get-responses-successful-activity-7062559356805918721-P9ry?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Muthukumar AE",
+  },
+  {
+    url: "https://www.linkedin.com/posts/trishlagupta24_licreatoraccelerator-linkedinforcreators-activity-6963492612666257408-K8Te?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Trishla Gupta",
+  },
+  {
+    url: "https://www.linkedin.com/posts/pankitparikh_coldemail-salesdevelopment-sdr-activity-7046222409636405248-AKyS?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Pankit Parikh",
+  },
+  {
+    url: "https://www.linkedin.com/posts/priya-vajpeyi_cold-emailing-activity-6968944925316907008-FWQV?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Priya Vajpeyi",
+  },
+  {
+    url: "https://www.linkedin.com/posts/muskanagarwall_writing-email-business-activity-7009374167665946625-KyVa?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Muskan Agarwall",
+  },
+  {
+    url: "https://www.linkedin.com/posts/dinesh-madanlal_how-to-write-cold-emails-for-sales-activity-7023514024067620864-cYfW?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Dinesh Madanlal",
+  },
+  {
+    url: "https://www.linkedin.com/posts/prathameshva-wordsmith_writing-contentwriter-engagement-activity-7054675697671225344-vxM4?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Prathamesh Vaidya",
+  },
+  {
+    url: "https://www.linkedin.com/posts/jatinchanchlani1_writing-a-formal-email-activity-6992133728428789760-uuWu?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Jatin Chanchlani",
+  },
+  {
+    url: "https://www.linkedin.com/posts/anicitamary-socialmediamanager_how-to-write-a-cold-email-like-a-pro-activity-7055049286417350656-KXum?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Anicita Mary",
+  },
+  {
+    url: "https://www.linkedin.com/posts/jayantpadhi_5-emil-subject-lines-activity-7056473146580746241-rDlr?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "Jayant Padhi",
+  },
+];
+const container = document.querySelector("#Gmail-Section");
+
+GmailArray.forEach((link) => {
+  const html = `
+  <section class="Resources-list" >
+      <div class="Listitem">
+        <div class="Source">
+          <a
+            href="${link.url}"
+            class="linked"
+            ><img src="../Images/URL.png" alt="" class="link"
+          /></a>
+          <p class="Discript">${link.description}</p>
+          <p class="free-paid" id="free">Free</p>
+        </div>
+        <div class="Discription-of-list">
+          <p class="credits">${link.credits}</p>
+        </div>
+      </div>
+      </section>
+  `;
+  container.insertAdjacentHTML("beforeend", html);
+});
--- Search.js
+++ Search.js
@@ -48,7 +48,3 @@ function performSearch() {
     alert(`Sorry, we couldn't find any items that match your search query`);
   }
 }
-
-function websiteVisits(response) {
-  document.querySelector("#visits").textContent = response.value;
-}
