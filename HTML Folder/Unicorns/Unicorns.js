465e4314c22504744753917bd6634e3a54fe4bce
Bharat Pahwa <pahwabharat15@gmail.com>
2023-05-15 19:02:33 +0530

    Added Unicorns and Off Campus

---
+++ HTML Folder/Unicorns/Unicorns.js
@@ -0,0 +1,24 @@
+const Unicorns = [{}];
+const container = document.querySelector("#Unicorns-Section");
+
+Unicorns.forEach((link) => {
+  const html = `
+  <section class="Resources-list" >
+      <div class="Listitem">
+        <div class="Source">
+          <a
+            href="${link.url}"
+            class="linked"
+            ><img src="../../Images/URL.png" alt="" class="link"
+          /></a>
+          <p class="Discript">${link.description}</p>
+          <p class="free-paid" id="free">Free</p>
+        </div>
+        <div class="Discription-of-list">
+          <p class="credits">${link.credits}</p>
+        </div>
+      </div>
+      </section>
+  `;
+  container.insertAdjacentHTML("beforeend", html);
+});
