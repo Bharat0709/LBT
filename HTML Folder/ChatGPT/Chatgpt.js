7d83f0daa3747c8f940e27c48b45c459b3044b81
Bharat Pahwa <pahwabharat15@gmail.com>
2023-05-17 21:38:22 +0530

    Added Description

--- HTML Folder/ChatGPT/Chatgpt.js
+++ HTML Folder/ChatGPT/Chatgpt.js
@@ -1,4 +1,286 @@
-const ChatGPT = [{}];
+const ChatGPT = [
+  {
+    url: "https://www.linkedin.com/posts/tulsisoni_most-effective-prompts-to-optimize-communication-activity-7060817530013052928-mUgt?utm_source=share&utm_medium=member_desktop",
+    description: "Most Effective Prompts to Optimize Communication",
+    credits: "Tulsi Soni",
+  },
+  {
+    url: "https://www.linkedin.com/posts/adityamalhotra6_the-simple-guide-to-chat-gpt-prompting-activity-7061334554258604033-HJVp?utm_source=share&utm_medium=member_desktop",
+    description: "The Simple Guide to Chat GPT Prompting",
+    credits: "Aditya Malhotra",
+  },
+  {
+    url: "https://www.linkedin.com/posts/neel-shah-553105178_chat-gpt-prompt-activity-7020379173407911936-GLOx?utm_source=share&utm_medium=member_desktop",
+    description: "Chat GPT Prompt",
+    credits: "Neel Shah",
+  },
+  {
+    url: "https://www.linkedin.com/feed/update/urn:li:activity:7047141167213268992?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://www.linkedin.com/posts/ashwin-palo-55a6a123_fb-ads-basic-chat-gpt-prompts-activity-7051995225229045760-kuaW?utm_source=share&utm_medium=member_desktop",
+    description: "FB Ads Basic Chat GPT Prompts",
+    credits: "Ashwin Palo",
+  },
+  {
+    url: "https://www.linkedin.com/posts/vidhuravinath_chatgpt-ai-aiprompts-activity-7060593609170960384-NROW?utm_source=share&utm_medium=member_desktop",
+    description: "ChatGPT AI AI Prompts",
+    credits: "Vidhu Ravi",
+  },
+  {
+    url: "https://www.linkedin.com/posts/sridhar-j_chatgpt-prompts-activity-7027209485316177920--UYc?utm_source=share&utm_medium=member_desktop",
+    description: "ChatGPT Prompts",
+    credits: "Sridhar J",
+  },
+  {
+    url: "https://www.linkedin.com/posts/abhijeet-nikam-23293a115_7-chat-gpt-prompts-you-dont-need-to-miss-activity-7031511418872946688-r3xx?utm_source=share&utm_medium=member_desktop",
+    description: "7 Chat GPT Prompts You Don't Need to Miss",
+    credits: "Abhijeet Nikam",
+  },
+  {
+    url: "https://www.linkedin.com/posts/kabeer-33_heres-the-formula-for-writing-100x-better-activity-7058688997199425536-7Dnc?utm_source=share&utm_medium=member_desktop",
+    description: "Here's the Formula for Writing 100x Better",
+    credits: "Kabeer",
+  },
+  {
+    url: "https://www.linkedin.com/posts/mohammedkaif19_you-might-be-doing-it-wrong-activity-7059807213065822208-kPjc?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/thejacksonyew/status/1654142212475609089?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/cyrilXBT/status/1654187014118318081?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/MushtaqBilalPhD/status/1654776633189085185?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/advertyofficial/status/1656997824389607429?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/shushant_l/status/1656652895838957572?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/sean0to10k/status/1655621430715854868?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/moritzkremb/status/1655943260739887110?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/AIValleyHQ/status/1655639744586604544?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/minchoi/status/1655600045217988609?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/BetterLegal/status/1656260371106152449?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/sharyph_/status/1655106890534141952?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/heyrobinai/status/1655998901625790466?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/Luke360/status/1656660495489961985?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/JonathanHerrick/status/1657081708578230272?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/rezkhere/status/1656336003756445696?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/MaximusJiang/status/1654879961520521217?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/thejacksonyew/status/1654142212475609089?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/cyrilXBT/status/1654187014118318081?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/MushtaqBilalPhD/status/1654776633189085185?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/advertyofficial/status/1656997824389607429?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/shushant_l/status/1656652895838957572?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/sean0to10k/status/1655621430715854868?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/moritzkremb/status/1655943260739887110?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/AIValleyHQ/status/1655639744586604544?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/minchoi/status/1655600045217988609?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/BetterLegal/status/1656260371106152449?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/sharyph_/status/1655106890534141952?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/heyrobinai/status/1655998901625790466?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/Luke360/status/1656660495489961985?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/JonathanHerrick/status/1657081708578230272?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/rezkhere/status/1656336003756445696?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/MaximusJiang/status/1654879961520521217?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://www.linkedin.com/posts/kabeer-33_how-to-write-killer-cold-emails-using-chat-activity-7057333921885167616-qmG6?utm_source=share&utm_medium=member_desktop",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/aaditsh/status/1654139726813691905?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/moritzkremb/status/1655268441031577601?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/TheRundownAI/status/1656361215101227009?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/heyrobinai/status/1654415687987736576?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/heyBarsee/status/1654511406916554753?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/heykahn/status/1656269623166382080?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/rowancheung/status/1654147381418508288?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/bowlescapital/status/1655569243159535618?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/aaditsh/status/1654941500596273153?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/AiHandbook/status/1656686354166697984?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/hasantoxr/status/1653667468143935488?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/TheJasSingh/status/1653905310061580288?s=20",
+    description: "",
+    credits: "",
+  },
+  {
+    url: "https://twitter.com/adcock_brett/status/1657030371412787213?s=20",
+    description: "",
+    credits: "",
+  },
+
+];
 const container = document.querySelector("#ChatGPT-Section");
 
 ChatGPT.forEach((link) => {
