fa68ae84d8f9f6a3469b4bf83cda10a512beda70
Bharat Pahwa <pahwabharat15@gmail.com>
2023-05-18 16:15:23 +0530

    Added Companies

--- HTML Folder/OFFCampus/OffCampus.js
+++ HTML Folder/OFFCampus/OffCampus.js
@@ -1,4 +1,238 @@
-const Companies = [{}];
+const Companies = [
+  {
+    url: "https://lnkd.in/dRyQ_rkk",
+    description: "Nagarro",
+  },
+  {
+    url: "https://lnkd.in/dHJwPXiG",
+    description: "Virtusa",
+  },
+  {
+    url: "https://lnkd.in/dUw9Qi4B",
+    description: "Zoho",
+  },
+  {
+    url: "https://lnkd.in/d3vs3whb",
+    description: "CGI",
+  },
+  {
+    url: "https://lnkd.in/dsXSfUev",
+    description: "Finastra",
+  },
+  {
+    url: "https://lnkd.in/dJCX6aVz",
+    description: "FIS",
+  },
+  {
+    url: "https://lnkd.in/d7inSReM",
+    description: "Fiserv",
+  },
+  {
+    url: "https://lnkd.in/dsxAXftw",
+    description: "IQVIA",
+  },
+  {
+    url: "https://lnkd.in/dqVxSNgW",
+    description: "JIO",
+  },
+  {
+    url: "https://lnkd.in/d2dkHExY",
+    description: "MAQ Software",
+  },
+  {
+    url: "https://lnkd.in/dvxb_7ds",
+    description: "Optum",
+  },
+  {
+    url: "https://lnkd.in/d6G3tHUF",
+    description: "Publicis Sapient",
+  },
+  {
+    url: "https://lnkd.in/dDKQVqv2",
+    description: "Geekyants",
+  },
+  {
+    url: "https://lnkd.in/dDN5PWQk",
+    description: "Accolite",
+  },
+  {
+    url: "https://lnkd.in/d9i9YwjV",
+    description: "Airtel",
+  },
+  {
+    url: "https://lnkd.in/dHTe2pFc",
+    description: "EA",
+  },
+  {
+    url: "https://lnkd.in/dgsH4KUz",
+    description: "Gartner",
+  },
+  {
+    url: "https://lnkd.in/dBP_hSFE",
+    description: "HARMAN",
+  },
+  {
+    url: "https://lnkd.in/dUPgitVf",
+    description: "Yellow[.]ai",
+  },
+  {
+    url: "https://lnkd.in/df4czTeb",
+    description: "Siemens",
+  },
+  {
+    url: "https://lnkd.in/d5gUrDxq",
+    description: "Samsung",
+  },
+  {
+    url: "https://lnkd.in/d7zgbhXk",
+    description: "Vmware",
+  },
+  {
+    url: "https://lnkd.in/dMWhmAKZ",
+    description: "Adobe",
+  },
+  {
+    url: "https://lnkd.in/dSYUatGR",
+    description: "Amazon",
+  },
+  {
+    url: "https://lnkd.in/dAjV2Df4",
+    description: "Cadence Design Systems",
+  },
+  {
+    url: "https://lnkd.in/dUNg4sZP",
+    description: "CleverTap",
+  },
+  {
+    url: "https://jobs.cisco.com/",
+    description: "Cisco",
+  },
+  {
+    url: "https://lnkd.in/d5ZUmmG6",
+    description: "Dunzo",
+  },
+  {
+    url: "https://apply.fampay.in/",
+    description: "FamPay",
+  },
+  {
+    url: "https://lnkd.in/d_9WfsNY",
+    description: "Flipkart",
+  },
+  {
+    url: "https://lnkd.in/dGMfCuRs",
+    description: "Google",
+  },
+  {
+    url: "https://lnkd.in/ds2n7SNb",
+    description: "Hackererath",
+  },
+  {
+    url: "https://lnkd.in/d53kRcp3",
+    description: "Morgan Stanley",
+  },
+  {
+    url: "https://lnkd.in/d9MbsS3V",
+    description: "EY",
+  },
+  {
+    url: "https://lnkd.in/d5pTjwxs",
+    description: "MyGate",
+  },
+  {
+    url: "https://lnkd.in/d7vST4g6",
+    description: "McAfee",
+  },
+  {
+    url: "https://lnkd.in/dDDbnZMu",
+    description: "Oracle",
+  },
+  {
+    url: "https://lnkd.in/dKt2drwp",
+    description: "Microsoft",
+  },
+  {
+    url: "https://lnkd.in/dtTZzhXn",
+    description: "PhonePe",
+  },
+  {
+    url: "https://lnkd.in/d4b8DTft",
+    description: "PWC",
+  },
+  {
+    url: "https://lnkd.in/dRuSSrq2",
+    description: "Rakuten",
+  },
+  {
+    url: "https://lnkd.in/dveHTU3p",
+    description: "Razorpay",
+  },
+  {
+    url: "https://lnkd.in/dDVKcPST",
+    description: "SAP",
+  },
+  {
+    url: "https://lnkd.in/dfti6QZ8",
+    description: "Media[.]net",
+  },
+  {
+    url: "https://lnkd.in/dskmG6eT",
+    description: "Twilio",
+  },
+  {
+    url: "https://lnkd.in/dX4g5UrW",
+    description: "Byju’s",
+  },
+  {
+    url: "https://lnkd.in/dJpHXdvv",
+    description: "TCS",
+  },
+  {
+    url: "https://lnkd.in/dEcdZ7gf",
+    description: "Infosys",
+  },
+  {
+    url: "https://lnkd.in/d89txDcp",
+    description: "Wipro",
+  },
+  {
+    url: "https://lnkd.in/d6tp6F_p",
+    description: "Cognizant",
+  },
+  {
+    url: "https://lnkd.in/dnCVuQzD",
+    description: "LTI",
+  },
+  {
+    url: "https://lnkd.in/dZBUYY88",
+    description: "Capgemini",
+  },
+  {
+    url: "https://lnkd.in/dnVzT7eb",
+    description: "DXC Technology",
+  },
+  {
+    url: "https://lnkd.in/dwTuQWAf",
+    description: "HCL",
+  },
+  {
+    url: "https://lnkd.in/d2ePnTG4",
+    description: "Hashedin",
+  },
+  {
+    url: "https://jobs.hexaware.com/",
+    description: "Hexaware",
+  },
+  { 
+    url: "https://lnkd.in/dtJkkrBp",
+    description: "Revature",
+  },
+  {
+    url: "https://lnkd.in/dU-VhUCw",
+    description: "IBM",
+  },
+];
+
 const container = document.querySelector("#OffCampus-Section");
 
 Companies.forEach((link) => {
@@ -15,7 +249,7 @@ Companies.forEach((link) => {
           <p class="free-paid" id="free">Free</p>
         </div>
         <div class="Discription-of-list">
-          <p class="credits">${link.credits}</p>
+          <p class="description"></p>
         </div>
       </div>
       </section>
