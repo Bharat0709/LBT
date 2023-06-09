6817338865f7222629ba4808a7abce3e88082daf
Bharat Pahwa <pahwabharat15@gmail.com>
2023-06-09 16:38:23 +0530

    added data Science

---
+++ HTML Folder/Backend Dev/Backend.js
@@ -0,0 +1,293 @@
+const containerRoadmaps = document.querySelector("#DSAinCRoadmaps");
+const containerVideoLectures = document.querySelector("#Video-Tutorials");
+const containerNotes = document.querySelector("#Notes");
+const containerQuestionSheet = document.querySelector("#Question-Sheet");
+const containerContentCreators = document.querySelector("#Content-Creators");
+const containerInterviewExperience = document.querySelector(
+  "#Interview-Experinece"
+);
+const RoadmapsArray = [
+  {
+    Url: "https://roadmap.sh/backend",
+    Cost: "free",
+    Description: "Backend Developer",
+    Credits: "Roadmap.sh",
+  },
+  {
+    Url: "https://www.knowledgehut.com/blog/web-development/backend-developer-roadmap",
+    Cost: "free",
+    Description: "Backend Developer Roadmap: The Ultimate Guide 2023",
+    Credits: "Knowledge Hut",
+  },
+  {
+    Url: "https://www.scaler.com/topics/software-engineering/backend-developer-roadmap/",
+    Cost: "free",
+    Description: "Backend Developer Roadmap: The Ultimate Guide 2023",
+    Credits: "Scaler.com",
+  },
+  {
+    Url: "https://www.geeksforgeeks.org/back-end-developer-roadmap-2022/",
+    Cost: "free",
+    Description: "Back-End Developer Roadmap 2022",
+    Credits: "Geeks for Geeks",
+  },
+  {
+    Url: "https://medium.com/codex/backend-developer-roadmap-e61ce30ab10d",
+    Cost: "free",
+    Description: "Backend Developer Roadmap",
+    Credits: "Prajix",
+  },
+  {
+    Url: "https://www.alexhyett.com/backend-developer-roadmap-2023/",
+    Cost: "free",
+    Description: "Backend Developer Roadmap 2023",
+    Credits: "ALEX HYETT",
+  },
+  {
+    Url: "https://dev.to/creativetim_official/the-complete-roadmap-for-becoming-a-backend-developer-step-by-step-instructions-d4j",
+    Cost: "free",
+    Description:
+      "The Complete Roadmap for Becoming a Backend Developer: Step-By-Step Instructions",
+    Credits: "Creative Tim",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=zwWROMFDBu0",
+    Cost: "free",
+    Description: "How To Become Backend Developer in 2022",
+    Credits: "Coding Ninjas",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=aZkG0b6oi4s",
+    Cost: "free",
+    Description: "How to Become a Backend Developer in 2023?",
+    Credits: "Codedamn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=1QI0wDJeXO4",
+    Cost: "free",
+    Description: "Complete Backend Development Roadmap",
+    Credits: "Anuj Bhaiya",
+  },
+];
+const VideoTutorials = [
+  {
+    Url: "https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/",
+    Cost: "paid",
+    Description: "Node.js, Express, MongoDB & More: The Complete Bootcamp 2023",
+    Credits: "Jonas Schmedtmann",
+  },
+  {
+    Url: "https://www.udemy.com/course/the-complete-web-development-bootcamp/",
+    Cost: "paid",
+    Description: "The Complete 2023 Web Development Bootcamp",
+    Credits: "Dr. Angela Yu",
+  },
+  {
+    Url: "https://www.udemy.com/course/software-developer-masterclass/",
+    Cost: "paid",
+    Description: "Complete Backend Development 2023 Bundle - Python Roadmap",
+    Credits: "Shubham Sarda",
+  },
+  {
+    Url: "https://www.udemy.com/course/django-python-advanced/",
+    Cost: "paid",
+    Description: "Build a Backend REST API with Python & Django - Advanced",
+    Credits: "Mark Winterbottom , Brooke Rutherford",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=w_L5XMp44tc",
+    Cost: "free",
+    Description: "Backend Development Full Course 2023",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=pmLnat8m4Nw",
+    Cost: "free",
+    Description: " Full Stack Development Full Course In 10 Hours",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=nu_pCVPKzTk",
+    Cost: "free",
+    Description: "Full Stack Web Development for Beginners",
+    Credits: "freecodecamp.org",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=BLl32FvcdVM",
+    Cost: "free",
+    Description: "Node Js Tutorial in Hindi",
+    Credits: "Code wih Harry",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=Oe421EPjeBE",
+    Cost: "free",
+    Description: "Node.js and Express.js - Full Course",
+    Credits: "freecodecamp.org",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=TlB_eWDSMt4",
+    Cost: "free",
+    Description: "Node.js Tutorial for Beginners: Learn Node in 1 Hour",
+    Credits: "Programming with Mosh",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=J6mDkcqU_ZE",
+    Cost: "free",
+    Description: "MongoDB Tutorial in 1 Hour (2023)",
+    Credits: "Code with Harry",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=SnqPyqRh4r4",
+    Cost: "free",
+    Description: "What Is MongoDB?",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=8eJJe4Slnik",
+    Cost: "free",
+    Description: "MongoDB Full Course 2023 ",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=SccSCuHhOw0",
+    Cost: "free",
+    Description: "Learn Express JS In 35 Minutes",
+    Credits: "Web Dev Simplified",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=0QRFOsrBtXw",
+    Cost: "free",
+    Description: "What Is Express JS?",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=XGa4onZP66Q",
+    Cost: "free",
+    Description: "What is an API ? Simply Explained",
+    Credits: "Apna College",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=JxzZxdht-XY",
+    Cost: "free",
+    Description: "Django Tutorial In Hindi",
+    Credits: "Code with Harry",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=rHux0gMZ3Eg",
+    Cost: "free",
+    Description: "Python Django Tutorial for Beginners",
+    Credits: "Programming with Mosh",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=BLrGJvFp75M",
+    Cost: "free",
+    Description: "Python Django Tutorial",
+    Credits: "Simplilearn",
+  },
+];
+const InterviewExperience = [{}];
+
+const InsertHtml = `
+<section class="Resources-list" >
+    <div class="Listitem">
+      <div class="Source">
+        <a
+          href="$\{Url}"
+          class="linked"
+          ><img src="../../Images/URL.png" alt="" class="link"
+        /></a>
+        <p class="Discript">$\{Description}</p>
+        <p class="free-paid" id="\${Cost}">\${Costp}</p>
+      </div>
+      <div class="Discription-of-list">
+        <p class="credits"> \${Credits}</p>
+      </div>
+    </div>
+</section>
+`;
+
+RoadmapsArray.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Costp = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase())
+    .replace(/\${Credits}/g, Credits);
+  containerRoadmaps.insertAdjacentHTML("beforeend", Replaced);
+});
+VideoTutorials.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
+    .replace(/\${Credits}/g, Credits);
+  containerVideoLectures.insertAdjacentHTML("beforeend", Replaced);
+});
+InterviewExperience.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
+    .replace(/\${Credits}/g, Credits);
+  containerInterviewExperience.insertAdjacentHTML("beforeend", Replaced);
+});
+QuestionSheet.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
+    .replace(/\${Credits}/g, Credits);
+  containerQuestionSheet.insertAdjacentHTML("beforeend", Replaced);
+});
+Notes.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
+    .replace(/\${Credits}/g, Credits);
+  containerNotes.insertAdjacentHTML("beforeend", Replaced);
+});
+ContentCreators.forEach((link) => {
+  const html = InsertHtml;
+  const Url = link.Url;
+  const Description = link.Description;
+  const Cost = link.Cost;
+  const Credits = link.Credits;
+  const Replaced = html
+    .replace(/\${Url}/g, Url)
+    .replace(/\${Description}/g, Description)
+    .replace(/\${Cost}/g, Cost)
+    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
+    .replace(/\${Credits}/g, Credits);
+  containerContentCreators.insertAdjacentHTML("beforeend", Replaced);
+});
--- HTML Folder/Data Science/Data.js
+++ HTML Folder/Data Science/Data.js
@@ -6,17 +6,248 @@ const containerContentCreators = document.querySelector("#Content-Creators");
 const containerInterviewExperience = document.querySelector(
   "#Interview-Experinece"
 );
-const RoadmapsArray = [{
-  Url:"www.google.com",
-  Cost:"Free",
-  Description:"Hello",
-  Credits:"Bharat Pahwa"
-}];
-const VideoTutorials = [{}];
-const Notes = [{}];
-const QuestionSheet = [{}];
-const InterviewExperience = [{}];
+const RoadmapsArray = [
+  {
+    Url: "https://www.scaler.com/topics/data-science/data-scientist-roadmap/",
+    Description: "A Complete Guide on Data Scientist Roadmap in 2023",
+    Cost: "free",
+    Credits: "Scaler",
+  },
+  {
+    Url: "https://www.analyticsvidhya.com/blog/2023/01/step-by-step-guide-to-become-a-data-scientist-in-2023/",
+    Description: "Step-by-step Guide To become a Data Scientist in 2023",
+    Cost: "free",
+    Credits: "Analytics Vidhya",
+  },
 
+  {
+    Url: " https://towardsdatascience.com/a-road-map-for-data-science-d1977504a72b",
+    Description: "A Road Map for Data Science",
+    Cost: "free",
+    Credits: "Towards Data Science",
+  },
+  {
+    Url: " https://github.com/Moataz-Elmesmary/Data-Science-Roadmap",
+    Description: "Data-Science-Roadmap",
+    Cost: "free",
+    Credits: "GitHub",
+  },
+  {
+    Url: " https://www.kdnuggets.com/2022/08/complete-data-science-study-roadmap.html",
+    Description: "The Complete Data Science Study Road Map",
+    Cost: "free",
+    Credits: "KDnuggets",
+  },
+  {
+    Url: "https://omdena.com/blog/data-science-road-map/",
+    Description: "DataScience Roadmap in 2023 – The Ultimate Guide",
+    Cost: "free",
+    Credits: "OmDena",
+  },
+  {
+    Url: "https://www.geeksforgeeks.org/how-to-become-data-scientist-a-complete-roadmap/",
+    Description: "A Road Map for Data Science – A Complete Roadmap",
+    Cost: "free",
+    Credits: "GeeksForGeeks",
+  },
+  {
+    Url: "https://www.simplilearn.com/data-science-roadmap-article#:~:text=What%20is%20the%20career%20path,data%20science%20career%20guide%20today.",
+    Description: "Data Science Roadmap 2023",
+    Cost: "free",
+    Credits: "SimpliLearn",
+  },
+];
+const Notes = [
+  {
+    Url: "https://www.geeksforgeeks.org/introduction-to-data-science/",
+    Description: "Introduction to Data Science",
+    Cost: "free",
+    Credits: "GeeksForGeeks",
+  },
+  {
+    Url: "https://nathancarter.github.io/MA346-course-notes/_build/html/chapter-1-intro-to-data-science.html",
+    Description: "What is Data Science",
+    Cost: "free",
+    Credits: "GitHub Pages",
+  },
+  {
+    Url: "https://www.guru99.com/data-science-tutorial.html",
+    Description:
+      "What is Data Science? Introduction, Basic Concepts and Processes",
+    Cost: "free",
+    Credits: "Guru99",
+  },
+  {
+    Url: "https://www.tutorialsduniya.com/notes/data-science-notes/",
+    Description: "Data Science Notes PDF",
+    Cost: "free",
+    Credits: "Tutorials Duniya",
+  },
+  {
+    Url: "http://www.datasciencecourse.org/lectures/",
+    Description: "Data Science Lectures",
+    Cost: "free",
+    Credits: "Practical Data Science",
+  },
+  {
+    Url: "https://www.javatpoint.com/data-science",
+    Description: "Data Science Tutorials for Beginners",
+    Cost: "free",
+    Credits: "Java Point",
+  },
+  {
+    Url: "https://sites.google.com/student.onlinedegree.iitm.ac.in/datasciencenotes/home",
+    Description: "Data Science Notes",
+    Cost: "free",
+    Credits: "Data Science Notes",
+  },
+  {
+    Url: "https://srdas.github.io/Papers/DSA_Book.pdf",
+    Description: "Data Science: Theories. Models, Algorithms and Analytics",
+    Cost: "free",
+    Credits: "Sanjiv Ranjan Das",
+  },
+  {
+    Url: "https://sist.sathyabama.ac.in/sist_coursematerial/uploads/SCSA3016.pdf",
+    Description: "Data Science",
+    Cost: "free",
+    Credits: "Sathyabama",
+  },
+  {
+    Url: "https://machinelearningprojects.net/handwritten-data-science-notes/",
+    Description: "Handwritten Data Science Notes",
+    Cost: "free",
+    Credits: "Machine Learning Projects by Abhishek Sharma",
+  },
+];
+const InterviewExperience = [
+  {
+    Url: "https://www.simplilearn.com/tutorials/data-science-tutorial/data-science-interview-questions",
+    Description: "Top 90+ Data Science Interview Questions and Answers 2023",
+    Cost: "free",
+    Credits: "Simplilearn",
+  },
+  {
+    Url: "https://intellipaat.com/blog/interview-question/data-science-interview-questions/",
+    Description: "Top 100+ Data Science Interview Questions in 2023",
+    Cost: "free",
+    Credits: "Intellipat",
+  },
+  {
+    Url: "https://www.interviewbit.com/data-science-interview-questions/",
+    Description: "Data Science Interview Questions",
+    Cost: "free",
+    Credits: "Interviewbit",
+  },
+  {
+    Url: "https://www.mygreatlearning.com/blog/data-science-interview-questions/",
+    Description: "100+ Data Science Interview Questions in 2023",
+    Cost: "free",
+    Credits: "great Learning",
+  },
+  {
+    Url: "https://www.edureka.co/blog/interview-questions/data-science-interview-questions/",
+    Description: "120+ Data Science Interview Questions And Answers for 2023",
+    Cost: "free",
+    Credits: "Edureka",
+  },
+  {
+    Url: "https://www.javatpoint.com/data-science-interview-questions",
+    Description: "Top 25 Data Science Interview Questions",
+    Cost: "free",
+    Credits: "Java Point",
+  },
+];
+
+const VideoTutorials = [
+  {
+    Url: "https://www.udemy.com/course/the-data-science-course-complete-data-science-bootcamp/",
+    Description: "The Data Science Course: Complete Data Science Bootcamp",
+    Cost: "paid",
+    Credits: "365 Careers Team",
+  },
+
+  {
+    Url: "https://www.udemy.com/course/python-for-data-science-and-machine-learning-bootcamp/",
+    Description: "Python for Data Science and Machine Learning Bootcamp",
+    Cost: "paid",
+    Credits: "Jose Portilla",
+  },
+  {
+    Url: "https://www.udemy.com/course/complete-machine-learning-and-data-science-zero-to-mastery/",
+    Description: "Complete Machine Learning & Data Science Bootcamp 2023",
+    Cost: "paid",
+    Credits: "Daniel Bourke , Andrei Neagoie",
+  },
+  {
+    Url: "https://www.udemy.com/course/datascience/#instructor-1",
+    Description: "Data Science A-Z™: Hands-On Exercises & ChatGPT Bonus [2023]",
+    Cost: "paid",
+    Credits: "Kirill Eremenko , SuperDataScience Team , Ligency Team",
+  },
+
+  {
+    Url: " https://www.youtube.com/watch?v=-ETQ97mXXF0",
+    Description: "Data Science Full Course – Learn Data Science in 10 hours",
+    Cost: "free",
+    Credits: "Edureka",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=JL_grPUnXzY&list=PLeo1K3hjS3us_ELKYSj_Fth2tIEkdKXvV",
+    Description: "Data Science Full Course For Beginners",
+    Cost: "free",
+    Credits: "codebasics",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=xiEC5oFsq2s",
+    Description: "Data Science Full Course – 12 hours",
+    Cost: "free",
+    Credits: "edureka",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=JDcZBzb46ts",
+    Description: "Python for Data Science Full Course",
+    Cost: "free",
+    Credits: "Great Learning",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=ua-CiDNNj30&list=PLWKjhJtqVAblQe2CCWqV4Zy3LY01Z8aF1",
+    Description: "Learn Data Science Tutorial – Full Course for Beginners",
+    Cost: "free",
+    Credits: "freeCodeCamp.org",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=X3paOmcrTjQ&list=PLEiEAq2VkUUIEQ7ENKU5Gv0HpRDtOphC6",
+    Description: "Data Science",
+    Cost: "free",
+    Credits: "SimpliLearn",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=dJjSQwKjuwE&list=PLG9aCp4uE-s2HRvW-fSs2E4hk07xWTGYB",
+    Description: "Data Science and Machine Learning",
+    Cost: "free",
+    Credits: "Unacademy Computer Science:",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=tMZn54M8tGU&list=PLfP3JxW-T70HvifebGl3d5d5jzI1Kp0i8",
+    Description:
+      "Machine Learning and Data Science Hamd-On Python Course Introduction",
+    Cost: "free",
+    Credits: "Indian AI Production",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=innk6tpRCW0",
+    Description: "Statistics for Data Science",
+    Cost: "free",
+    Credits: "Great Learning",
+  },
+  {
+    Url: "https://www.youtube.com/watch?v=jtn-hRJjl68",
+    Description: "What is Data Science? Complete RoadMap",
+    Cost: "free",
+    Credits: "Apna College",
+  },
+];
 const InsertHtml = `
 <section class="Resources-list" >
     <div class="Listitem">
@@ -79,20 +310,6 @@ InterviewExperience.forEach((link) => {
     .replace(/\${Credits}/g, Credits);
   containerInterviewExperience.insertAdjacentHTML("beforeend", Replaced);
 });
-QuestionSheet.forEach((link) => {
-  const html = InsertHtml;
-  const Url = link.Url;
-  const Description = link.Description;
-  const Cost = link.Cost;
-  const Credits = link.Credits;
-  const Replaced = html
-    .replace(/\${Url}/g, Url)
-    .replace(/\${Description}/g, Description)
-    .replace(/\${Cost}/g, Cost)
-    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
-    .replace(/\${Credits}/g, Credits);
-  containerQuestionSheet.insertAdjacentHTML("beforeend", Replaced);
-});
 Notes.forEach((link) => {
   const html = InsertHtml;
   const Url = link.Url;
@@ -106,18 +323,4 @@ Notes.forEach((link) => {
     .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
     .replace(/\${Credits}/g, Credits);
   containerNotes.insertAdjacentHTML("beforeend", Replaced);
-});
-ContentCreators.forEach((link) => {
-  const html = InsertHtml;
-  const Url = link.Url;
-  const Description = link.Description;
-  const Cost = link.Cost;
-  const Credits = link.Credits;
-  const Replaced = html
-    .replace(/\${Url}/g, Url)
-    .replace(/\${Description}/g, Description)
-    .replace(/\${Cost}/g, Cost)
-    .replace(/\${Costp}/g, Cost.charAt(0).toUpperCase() + Cost.slice(1))
-    .replace(/\${Credits}/g, Credits);
-  containerContentCreators.insertAdjacentHTML("beforeend", Replaced);
 });
